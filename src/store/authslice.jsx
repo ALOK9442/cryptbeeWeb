24fa14b5d8935941170b9ced63bc396bd9e41153
ALOK9442 <alok2110036@akgec.ac.in>
2023-10-24 01:48:44 +0530

    feature: store and authSlice

---
+++ src/store/authslice.jsx
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const initialState = {
+    user: null,
+    token: null,
+    isAuth: false,
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers:{
+        login(state,action){
+            state.user = action.payload.user
+            state.token = action.payload.token
+            state.isAuth = true
+        },
+        logout(state){
+            state.user = null
+            state.token = null
+            state.isAuth = false
+        }
+    }
+})
+
+export const {login,logout} = authSlice.actions
+
+export default authSlice.reducer
