1cb25ce0182301df1cc5627bc3c872ae4839df26
ALOK9442 <alok2110036@akgec.ac.in>
2023-10-24 01:30:00 +0530

    initial commit

---
+++ src/app.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './app.css'
+
+function App() {
+
+  
+  return (
+    <>
+      <div className='text-3xl font-bold underline'>
+        CryptBee
+      </div>
+    </>
+  )
+}
+
+export default App
